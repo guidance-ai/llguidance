e7c472ad77644da3c8e0f72c8d074ef61902efa2
Michal Moskal <michal@moskal.me>
2025-04-14 16:36:54 -0700

    fmt

--- parser/src/json/compiler.rs
+++ parser/src/json/compiler.rs
@@ -200,7 +200,8 @@ impl Compiler {
 
     fn process_one_of(&mut self, options: &[Schema]) -> Result<NodeRef> {
         if self.options.coerce_one_of || self.options.lenient {
-            self.builder.add_warning("oneOf coerced to anyOf".to_string());
+            self.builder
+                .add_warning("oneOf coerced to anyOf".to_string());
             self.process_any_of(options)
         } else {
             Err(anyhow!("oneOf constraints are not supported. Enable 'coerce_one_of' option to approximate oneOf with anyOf"))
