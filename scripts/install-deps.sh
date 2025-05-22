41abc2050410b94b52721e77f2618d30ccd096e1
Michal Moskal <michal@moskal.me>
2025-05-22 10:13:06 -0700

    Revert "avoid compiling llama cpp"
    This reverts commit f68624d09717bcca7361846fe73b7ef6b8d837ca.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,8 +2,7 @@
 
 # installing guidance for deps
 pip install pytest guidance huggingface_hub tokenizers jsonschema maturin[zig] \
-    torch transformers==4.52.1 bitsandbytes ipython psutil mypy llama_cpp_python \
-    --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cpu
+    torch transformers==4.52.1 bitsandbytes ipython psutil mypy llama_cpp_python
 pip uninstall -y guidance
 
 # print out versions
