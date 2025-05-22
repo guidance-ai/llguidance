f68624d09717bcca7361846fe73b7ef6b8d837ca
Michal Moskal <michal@moskal.me>
2025-05-22 10:03:52 -0700

    avoid compiling llama cpp

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,7 +2,8 @@
 
 # installing guidance for deps
 pip install pytest guidance huggingface_hub tokenizers jsonschema maturin[zig] \
-    torch transformers==4.52.1 bitsandbytes ipython psutil mypy llama_cpp_python
+    torch transformers==4.52.1 bitsandbytes ipython psutil mypy llama_cpp_python \
+    --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cpu
 pip uninstall -y guidance
 
 # print out versions
