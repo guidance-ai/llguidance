4472d500453b6b94d4a249d58b654f6825563bfc
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-07-21 09:34:55 -0400

    More array testing

--- sample_parser/tests/test_json_arrays.rs
+++ sample_parser/tests/test_json_arrays.rs
@@ -10,6 +10,12 @@ fn test_json_array() {
         &[json!([1, 2, 3]), json!([]), json!([1])],
         &[json!([1, "Hello"]), json!([true, false]), json!([1.0, 2.0])],
     );
+    // Just prove we can do other primitive types in arrays
+    json_test_many(
+        &json!({"type":"array", "items": {"type":"boolean"}}),
+        &[json!([true, false, true]), json!([]), json!([true])],
+        &[json!([1, "Hello"]), json!([1, 2]), json!([1.0, 2.0])],
+    );
 }
 
 #[test]
@@ -22,5 +28,39 @@ fn test_json_array_length_constraints() {
     json_err_test(
         &json!({"type":"array", "items": {"type":"integer"}, "minItems": 2, "maxItems": 1}),
         "Unsatisfiable schema",
+    );
+}
+
+#[test]
+fn test_json_array_nested() {
+    json_test_many(
+        &json!({"type":"array", "items": {"type":"array", "items": {"type":"integer"}}}),
+        &[
+            json!([]),
+            json!([[1]]),
+            json!([[1], []]),
+            json!([[], [1]]),
+            json!([[1, 2], [3, 4]]),
+            json!([[0], [1, 2, 3]]),
+            json!([[0], [1, 2, 3], [4, 5]]),
+        ],
+        &[
+            json!([[1, "Hello"]]),
+            json!([[true, false]]),
+            json!([[1.0, 2.0]]),
+        ],
+    );
+}
+
+#[test]
+fn test_json_array_objects() {
+    json_test_many(
+        &json!({"type":"array", "items": {"type":"object", "properties": {"a": {"type":"integer"}}, "required": ["a"]}}),
+        &[json!([]), json!([{"a": 1}]), json!([{"a": 1}, {"a": 2}])],
+        &[
+            json!([{"b": 1}]),
+            json!([{"a": "Hello"}]),
+            json!([{"a": 1}, {"b": 2}]),
+        ],
     );
 }
