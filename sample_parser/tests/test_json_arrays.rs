82eed80e5a0a315e916a40f9022a7a7da0e8b76b
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-07-22 14:47:45 -0400

    Some renaming

--- sample_parser/tests/test_json_arrays.rs
+++ sample_parser/tests/test_json_arrays.rs
@@ -8,7 +8,7 @@ use common_lark_utils::{json_err_test, json_schema_check};
 #[case(&json!([]),)]
 #[case(&json!([1]),)]
 #[case(&json!([1, 2, 3]),)]
-fn test_json_array_integer(#[case] sample_array: &Value) {
+fn array_integer(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"integer"}});
     json_schema_check(schema, sample_array, true);
 }
@@ -16,7 +16,7 @@ fn test_json_array_integer(#[case] sample_array: &Value) {
 #[case(&json!([1, "Hello"]),)]
 #[case(&json!([true, false]),)]
 #[case(&json!([1.0, 3.0]),)]
-fn test_json_array_integer_failures(#[case] sample_array: &Value) {
+fn array_integer_failures(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"integer"}});
     json_schema_check(schema, sample_array, false);
 }
@@ -26,7 +26,7 @@ fn test_json_array_integer_failures(#[case] sample_array: &Value) {
 #[case(&json!([true]),)]
 #[case(&json!([false]),)]
 #[case(&json!([false, true]),)]
-fn test_json_array_boolean(#[case] sample_array: &Value) {
+fn array_boolean(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"boolean"}});
     json_schema_check(schema, sample_array, true);
 }
@@ -34,7 +34,7 @@ fn test_json_array_boolean(#[case] sample_array: &Value) {
 #[case(&json!([true, 0]),)]
 #[case(&json!([false, 1]),)]
 #[case(&json!([1.0, 0.0]),)]
-fn test_json_array_boolean_failures(#[case] sample_array: &Value) {
+fn array_boolean_failures(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"boolean"}});
     json_schema_check(schema, sample_array, false);
 }
@@ -43,7 +43,7 @@ fn test_json_array_boolean_failures(#[case] sample_array: &Value) {
 #[case(&json!([1,2]))]
 #[case(&json!([1,2, 3]))]
 #[case(&json!([1,2, 3, 4]))]
-fn test_json_array_length(#[case] sample_array: &Value) {
+fn array_length_constraints(#[case] sample_array: &Value) {
     let schema =
         &json!({"type":"array", "items": {"type":"integer"}, "minItems": 2, "maxItems": 4});
     json_schema_check(schema, sample_array, true);
@@ -53,14 +53,14 @@ fn test_json_array_length(#[case] sample_array: &Value) {
 #[case(&json!([]))]
 #[case(&json!([1]))]
 #[case(&json!([1,2,3,4,5]))]
-fn test_json_array_length_failures(#[case] sample_array: &Value) {
+fn array_length_failures(#[case] sample_array: &Value) {
     let schema =
         &json!({"type":"array", "items": {"type":"integer"}, "minItems": 2, "maxItems": 4});
     json_schema_check(schema, sample_array, false);
 }
 
 #[test]
-fn test_json_array_length_bad_constraints() {
+fn array_length_bad_constraints() {
     json_err_test(
         &json!({"type":"array", "items": {"type":"integer"}, "minItems": 2, "maxItems": 1}),
         "Unsatisfiable schema: minItems (2) is greater than maxItems (1)",
@@ -75,7 +75,7 @@ fn test_json_array_length_bad_constraints() {
 #[case(&json!([[1, 2], [3, 4]]))]
 #[case(&json!([[0], [1, 2, 3]]))]
 #[case(&json!([[0], [1, 2, 3], [4, 5]]))]
-fn test_json_nested_array(#[case] sample_array: &Value) {
+fn nested_array(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"array", "items": {"type":"integer"}}});
     json_schema_check(schema, sample_array, true);
 }
@@ -84,7 +84,7 @@ fn test_json_nested_array(#[case] sample_array: &Value) {
 #[case(&json!([[1, "Hello"]]))]
 #[case(&json!([[true, false]]))]
 #[case(&json!([[1.0, 2.0]]))]
-fn test_json_nested_array_failures(#[case] sample_array: &Value) {
+fn nested_array_failures(#[case] sample_array: &Value) {
     let schema = &json!({"type":"array", "items": {"type":"array", "items": {"type":"integer"}}});
     json_schema_check(schema, sample_array, false);
 }
@@ -93,7 +93,7 @@ fn test_json_nested_array_failures(#[case] sample_array: &Value) {
 #[case(&json!([]))]
 #[case(&json!([{"a": 1}]))]
 #[case(&json!([{"a": 1}, {"a": 2}]))]
-fn test_json_array_of_objects(#[case] sample_array: &Value) {
+fn array_of_objects(#[case] sample_array: &Value) {
     let schema = &json!(
         {
             "type":"array",
@@ -115,7 +115,7 @@ fn test_json_array_of_objects(#[case] sample_array: &Value) {
 #[case(&json!([{"b": 1}]))]
 #[case(&json!([{"a": "Hello"}]))]
 #[case(&json!([{"a": 1}, {"b": 2}]))]
-fn test_json_array_of_objects_failures(#[case] sample_array: &Value) {
+fn array_of_objects_failures(#[case] sample_array: &Value) {
     let schema = &json!(
         {
             "type":"array",
