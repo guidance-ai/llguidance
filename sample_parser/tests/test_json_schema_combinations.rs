dfa7fe9709d81386a6fd1d63012219b9be17fa12
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-07-30 12:01:21 -0400

    Port another allof

--- sample_parser/tests/test_json_schema_combinations.rs
+++ sample_parser/tests/test_json_schema_combinations.rs
@@ -46,3 +46,40 @@ fn simple_allof_failures(
 ) {
     json_schema_check(&SIMPLE_ALLOF, &sample, false);
 }
+
+lazy_static! {
+    static ref ALLOF_WITH_BASE: Value = json!({
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "properties": {"bar": {"type": "integer"}},
+            "required": ["bar"],
+            "allOf": [
+                {"properties": {"foo": {"type": "string"}}, "required": ["foo"]},
+                {"properties": {"baz": {"type": "null"}}, "required": ["baz"]},
+            ],
+    });
+}
+
+#[rstest]
+#[case(&json!({"bar": 2, "foo": "quux", "baz": null}), true)]
+#[case(&json!({"foo": "quux", "baz": null}), false)]
+#[case(&json!({"bar": 2, "baz": null}), false)]
+#[case(&json!({"bar": 2, "foo": "quux"}), false)]
+#[case(&json!({"bar": 2}), false)]
+fn allof_with_base(#[case] sample: &Value, #[case] expected_pass: bool) {
+    json_schema_check(&ALLOF_WITH_BASE, sample, expected_pass);
+}
+
+#[rstest]
+#[case(-35, false)]
+#[case(0, false)]
+#[case(29, false)]
+#[case(30, true)]
+#[case(35, true)]
+#[case(381925, true)]
+fn allof_simple_minimum(#[case] value: i32, #[case] expected_pass: bool) {
+    let schema = json!({
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "allOf": [{"minimum": 30}, {"minimum": 20}],
+    });
+    json_schema_check(&schema, &json!(value), expected_pass);
+}
