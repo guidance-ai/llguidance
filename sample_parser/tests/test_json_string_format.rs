d11d4ae200d8621ae5a2928fe6ccab8fec9e2bd6
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-08-12 13:10:51 -0400

    Fill out some more cases

--- sample_parser/tests/test_json_string_format.rs
+++ sample_parser/tests/test_json_string_format.rs
@@ -15,10 +15,24 @@ pub fn valid_date_time(#[case] value: &Value) {
 }
 
 #[rstest]
+#[case(&json!("1963-06-38T08:30:06.283185Z"))]
+pub fn bad_date_time(#[case] value: &Value) {
+    let schema = json!({"type":"string", "format":"date-time"});
+    json_schema_check(&schema, &value, false);
+}
+
+#[rstest]
 #[case("08:30:06.283185Z")]
 pub fn valid_time(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"time"});
     json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("28:30:06.283185Z")]
+pub fn bad_time(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"time"});
+    json_schema_check(&schema, &json!(s), false);
 }
 
 #[rstest]
@@ -29,10 +43,24 @@ pub fn valid_date(#[case] s: &str) {
 }
 
 #[rstest]
+#[case("1963-13-19")]
+pub fn bad_date(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"date"});
+    json_schema_check(&schema, &json!(s), false);
+}
+
+#[rstest]
 #[case("P1M")]
 pub fn valid_duration(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"duration"});
     json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("P2D1Y")]
+pub fn bad_duration(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"duration"});
+    json_schema_check(&schema, &json!(s), false);
 }
 
 #[rstest]
@@ -43,10 +71,24 @@ pub fn valid_email(#[case] s: &str) {
 }
 
 #[rstest]
+#[case("joe.bloggs@@example.com")]
+pub fn bad_email(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"email"});
+    json_schema_check(&schema, &json!(s), false);
+}
+
+#[rstest]
 #[case("hostnam3")]
 pub fn valid_hostname(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"hostname"});
     json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("hostnam3-")]
+pub fn bad_hostname(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"hostname"});
+    json_schema_check(&schema, &json!(s), false);
 }
 
 #[rstest]
@@ -57,6 +99,13 @@ pub fn valid_ipv4(#[case] s: &str) {
 }
 
 #[rstest]
+#[case("192.168.0.0.1")]
+pub fn bad_ipv4(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"ipv4"});
+    json_schema_check(&schema, &json!(s), false);
+}
+
+#[rstest]
 #[case("::42:ff:1")]
 pub fn valid_ipv6(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"ipv6"});
@@ -64,10 +113,24 @@ pub fn valid_ipv6(#[case] s: &str) {
 }
 
 #[rstest]
+#[case("1:1:1:1:1:1:1:1:1:1:1:1:1:1:1:1")]
+pub fn bad_ipv6(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"ipv6"});
+    json_schema_check(&schema, &json!(s), false);
+}
+
+#[rstest]
 #[case("2eb8aa08-AA98-11ea-B4Aa-73B441D16380")]
 pub fn valid_uuid(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"uuid"});
     json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("2eb8-aa08-aa98-11ea-b4aa73b44-1d16380")]
+pub fn bad_uuid(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"uuid"});
+    json_schema_check(&schema, &json!(s), false);
 }
 
 #[rstest]
