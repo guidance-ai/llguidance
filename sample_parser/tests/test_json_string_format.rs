a17abcbe7f2c28f71c3bbbca012d06e58e6b7f17
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-08-12 13:03:02 -0400

    More formats

--- sample_parser/tests/test_json_string_format.rs
+++ sample_parser/tests/test_json_string_format.rs
@@ -1,5 +1,5 @@
 // This is for testing JSON string formats
-// Only smoke testing for now
+// Only smoke testing for now; more comprehensive tests are in Python
 
 use rstest::*;
 use serde_json::{json, Value};
@@ -39,5 +39,33 @@ pub fn valid_duration(#[case] s: &str) {
 #[case("joe.bloggs@example.com")]
 pub fn valid_email(#[case] s: &str) {
     let schema = json!({"type":"string", "format":"email"});
+    json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("hostnam3")]
+pub fn valid_hostname(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"hostname"});
+    json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("192.168.0.1")]
+pub fn valid_ipv4(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"ipv4"});
+    json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("::42:ff:1")]
+pub fn valid_ipv6(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"ipv6"});
+    json_schema_check(&schema, &json!(s), true);
+}
+
+#[rstest]
+#[case("2eb8aa08-AA98-11ea-B4Aa-73B441D16380")]
+pub fn valid_uuid(#[case] s: &str) {
+    let schema = json!({"type":"string", "format":"uuid"});
     json_schema_check(&schema, &json!(s), true);
 }
